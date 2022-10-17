e94077d617481d1703d3e95bb7fd348cf55eee3a
Vitor Hugo Schiphorst <vitorhugo.schiphorst@gmail.com>
2022-10-17 15:02:09 -0300

    Prática 17/10

---
+++ pratica-final/src/ifsc/Animal.java
@@ -0,0 +1,60 @@
+package ifsc;
+
+public class Animal extends AnimalMain {
+
+	private String nome;
+	private String raca;
+	private Float comp;
+	private Integer patas;
+	private String cor;
+	private String ecos;
+
+	public Float getComp() {
+		return comp;
+	}
+
+	public void setComp(Float comp) {
+		this.comp = comp;
+	}
+
+	public Integer getPatas() {
+		return patas;
+	}
+
+	public void setPatas(Integer patas) {
+		this.patas = patas;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public String getEcos() {
+		return ecos;
+	}
+
+	public void setEcos(String ecos) {
+		this.ecos = ecos;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRaca() {
+		return raca;
+	}
+
+	public void setRaca(String raca) {
+		this.raca = raca;
+	}
+
+}
